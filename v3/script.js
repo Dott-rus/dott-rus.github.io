5029e6912e567de1979392593314c20561e1f3ab
Dott-rus <78660260+Dott-rus@users.noreply.github.com>
2023-03-07 10:05:40 +0300

    v3

---
+++ v3/script.js
@@ -0,0 +1,50 @@
+const letters = "AB[]CD>EFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./[]<";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 7;
+  }, 30);
+};
+
+let index = 0,
+  intervall = 1000;
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = (star) => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+};
+
+for (const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+
+    setIntervall(() => animate(star), 1000);
+  }, index++ * (intervall / 3));
+}
