e293904fc43685e1c2b45658477c89005e693f8f
Dott-rus <antonamelin8@gmail.com>
2023-11-07 11:33:57 +0300

    some updates

---
+++ v2/js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2, 
+
+  // Заголовок
+  title: 'Dott',
+
+  // Социальные сети
+  social: {
+    telegram: '@Adott0',
+    steam: 'dott__',
+    vk: '0dott0', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#DB16DB',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#F20C7F',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#B30CF2',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'by Dott',
+  // Цвет копирайта
+  copyrightTextColor: '#700CE8',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#E80C0C',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
